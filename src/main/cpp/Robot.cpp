790ffa6b4656412062fda43cc5ea3e0f2a4eb61d
Mahdi <42314845+OofRekt@users.noreply.github.com>
2022-01-13 12:39:24 -0800

    Update Robot.cpp

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,14 +1,22 @@
-#include "Robot.h"
-#include <fmt/core.h>
+#include <Robot.h>
 #include <frc/smartdashboard/SmartDashboard.h>
 #include "rev/CANSparkMax.h"
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableInstance.h"
+
 
 double leftleadmotorID = 3, rightleadmotorID = 1, leftfollowmotorID = 4 , rightfollowermotorID = 2;
- rev::CANSparkMax m_leftfront{leftleadmotorID, rev::CANSparkMax::MotorType::kBrushless};
- rev::CANSparkMax m_leftback{leftfollowmotorID, rev::CANSparkMax::MotorType::kBrushless};
- rev::CANSparkMax m_rightfront{rightleadmotorID, rev::CANSparkMax::MotorType::kBrushless};
- rev::CANSparkMax m_rightback{rightfollowermotorID, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_leftfront{leftleadmotorID, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_leftback{leftfollowmotorID, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_rightfront{rightleadmotorID, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_rightback{rightfollowermotorID, rev::CANSparkMax::MotorType::kBrushless};
 
+  std::shared_ptr<NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
+  double tx = table->GetNumber("tx",0.0);                   //Get horizontal off set from target
+  double ty = table->GetNumber("ty",0.0);                   //Get vertical offset from target
+  double ta = table->GetNumber("ta",0.0);                   //Get area of target on screen
+  double ts = table->GetNumber("ts",0.0);                   //Get skew of target
+  double tv = table->GetNumber("tv", 0.0);
 
 void Robot::RobotInit() {
   m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
@@ -22,16 +30,29 @@ void Robot::RobotInit() {
 void Robot::RobotPeriodic() {}
 
 void Robot::AutonomousInit() {
+  while (true){
+    std::shared_ptr<NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
+    double tx = table->GetNumber("tx",0.0);  
+    float Kp = -0.1;
+
+    float heading_error = tx;
+    float steering_adjust = Kp * tx;
+
+    leftside += steering_adjust;
+    rightside -= steering_adjust;
+  }
+  /*
   m_autoSelected = m_chooser.GetSelected();
   // m_autoSelected = SmartDashboard::GetString("Auto Selector",
   //     kAutoNameDefault);
-  fmt::print("Auto selected: {}\n", m_autoSelected);
+  // fmt::print("Auto selected: {}\n", m_autoSelected);
 
   if (m_autoSelected == kAutoNameCustom) {
     // Custom Auto goes here
   } else {
     // Default Auto goes here
   }
+  */
 }
 
 void Robot::AutonomousPeriodic() {
