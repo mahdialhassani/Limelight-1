bfd05d50a9d2f95a728e940d5e08dc4d510ba807
Mahdi <42314845+OofRekt@users.noreply.github.com>
2022-01-14 12:05:26 -0800

    commit

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -3,9 +3,11 @@
 #include "rev/CANSparkMax.h"
 #include "networktables/NetworkTable.h"
 #include "networktables/NetworkTableInstance.h"
+#include <frc/Joystick.h>
 
+using namespace frc;
 
-double leftleadmotorID = 3, rightleadmotorID = 1, leftfollowmotorID = 4 , rightfollowermotorID = 2;
+  double leftleadmotorID = 3, rightleadmotorID = 1, leftfollowmotorID = 4 , rightfollowermotorID = 2;
   rev::CANSparkMax m_leftfront{leftleadmotorID, rev::CANSparkMax::MotorType::kBrushless};
   rev::CANSparkMax m_leftback{leftfollowmotorID, rev::CANSparkMax::MotorType::kBrushless};
   rev::CANSparkMax m_rightfront{rightleadmotorID, rev::CANSparkMax::MotorType::kBrushless};
@@ -18,11 +20,8 @@ double leftleadmotorID = 3, rightleadmotorID = 1, leftfollowmotorID = 4 , rightf
   double ts = table->GetNumber("ts",0.0);                   //Get skew of target
   double tv = table->GetNumber("tv", 0.0);
 
-  frc::Encoder leftEncoder{3,4;
-  frc::Encoder rightEncoder{1,2};
-
-  //frc::MotorControllerGroup leftside{m_leftfront, m_leftback};
-  //frc::MotorControllerGroup rightside{m_rightfront, m_rightback};
+  rev::CANEncoder m_encoderR = m_rightfront.GetEncoder();
+  rev::CANEncoder m_encoderL = m_leftfront.GetEncoder();
 
   frc::DifferentialDrive drive{m_leftfront, m_rightfront}
 
@@ -31,32 +30,22 @@ void Robot::RobotInit() {
   m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
   frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
 
+  m_stick = new Joystick(0);
+
   m_leftback.Follow(m_leftfront);
   m_rightback.Follow(m_rightfront);
+
+  m_encoderR.SetPosition(0.0);
+  m_encoderL.SetPosition(0.0);
 
   //drives 18.85 inches per rotation; 8.68 motor revs * 42
   //365 rotations for 18.85 inches
 
-  leftEncoder.SetDistancePerPulse(1./256.);
-  rightEncoder.SetDistancePerPulse(1./256.);
 }
 
 void Robot::RobotPeriodic() {}
 
 void Robot::AutonomousInit() {
-  while (true){
-    std::shared_ptr<NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
-    double tx = table->GetNumber("tx",0.0);  
-    float Kp = -0.1;
-
-    float heading_error = tx;
-    float steering_adjust = Kp * tx;
-
-    drive.tankDrive(steering_adjust, -steering_adjust);
-
-    //m_leftfront += steering_adjust;
-    //m_rightfront -= steering_adjust;
-  }
   /*
   m_autoSelected = m_chooser.GetSelected();
   // m_autoSelected = SmartDashboard::GetString("Auto Selector",
@@ -81,7 +70,29 @@ void Robot::AutonomousPeriodic() {
 
 void Robot::TeleopInit() {}
 
-void Robot::TeleopPeriodic() {}
+void Robot::TeleopPeriodic() {
+
+  float Kp = -0.1;
+
+  std::shared_ptr<NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
+  double tx = table->GetNumber("tx");  
+
+  if (m_stick->GetRawButtonPressed(2)){
+
+    float heading_error = tx;
+    float steering_adjust = Kp * tx;
+
+    //float rotations = (steering_adjust/(6*pi))*8.68;
+
+    m_leftfront.Set(steering_adjust);
+    m_rightfront.Set(-steering_adjust);
+
+    //drive.tankDrive(steering_adjust, -steering_adjust);
+
+    //m_leftfront += steering_adjust;
+    //m_rightfront -= steering_adjust;
+  }
+}
 
 void Robot::DisabledInit() {}
 
